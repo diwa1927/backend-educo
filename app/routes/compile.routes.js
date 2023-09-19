7709be9daf9f2180e46e0a504d7429c780d3d4c5
diwa1927 <hadiwardana90@gmail.com>
2023-09-19 13:43:25 +0700

    commits

---
+++ app/routes/compile.routes.js
@@ -0,0 +1,29 @@
+module.exports = function(app) {
+    app.post("/compile", async (req, res) => {
+        let code = req.body.code;
+        let userInput = req.body.userInput;
+        let data = {
+            files: [
+            {
+                name: `main.cpp`,
+                content: code,
+            },
+            ],
+            stdin: userInput,
+        };
+        let config = {
+            method: "post",
+            url: `https://glot.io/api/run/cpp/latest`,
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `Token 40f4948c-6825-417d-9038-6eaba9f89e35`,
+            },
+            data: data,
+        };
+        await axios(config).then((response) => {
+            return res.status(200).json({ status: 200, data: response.data });
+            }).catch(() => {
+            return res.status(500).json({ error: "Internal Server Error" });
+        });
+    });
+};
--- server.js
+++ server.js
@@ -30,37 +30,10 @@ app.get('/', (req,res) => {
     res.json({ message: "Welcome to Database Educo, database is connected!"})
 });
 
-app.post("/compile", async (req, res) => {
-    let code = req.body.code;
-    let userInput = req.body.userInput;
-    let data = {
-        files: [
-        {
-            name: `main.cpp`,
-            content: code,
-        },
-        ],
-        stdin: userInput,
-    };
-    let config = {
-        method: "post",
-        url: `https://glot.io/api/run/cpp/latest`,
-        headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token 40f4948c-6825-417d-9038-6eaba9f89e35`,
-        },
-        data: data,
-    };
-    await axios(config).then((response) => {
-        return res.status(200).json({ status: 200, data: response.data });
-        }).catch(() => {
-        return res.status(500).json({ error: "Internal Server Error" });
-    });
-});
-
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 require('./app/routes/assignment.routes')(app);
+require('./app/routes/compile.routes')(app);
 
 app.listen(PORT, () => {
     console.log(` Server listening on port ${PORT} `);
