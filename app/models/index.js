a94f106d885797dc16dfd6a5328b28a803c21f7f
diwa1927 <hadiwardana90@gmail.com>
2023-10-23 17:01:52 +0700

    commits

--- app/models/index.js
+++ app/models/index.js
@@ -24,7 +24,6 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
-db.assignment = require("../models/assignment.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles"
@@ -34,14 +33,5 @@ db.user.belongsToMany(db.role, {
 });
 
 db.ROLES = ["siswa", "admin", "guru"];
-
-db.assignment.belongsToMany(db.user, {
-  through: "user_assignments"
-});
-db.user.belongsToMany(db.assignment, {
-  through: "user_assignments"
-});
-
-db.ASSIGNMENTS = ["Tipe Data","Operator","Percabangan","Perulangan","Array"]
 
 module.exports = db;
--- app/routes/assignment.routes.js
+++ app/routes/assignment.routes.js
@@ -1,13 +1,56 @@
-const controller = require("../controllers/assignment.controller");
-
 module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
+    const {google} = require("googleapis");
+    const axios = require("axios");
+    const fs = require("fs")
+
+    fs.readFile('credentials.json', (err, data) => {
+        if (err) {
+            console.error('Terjadi kesalahan.', err);
+            return;
+        }
+
+        JSON.parse(data);
     });
 
-    app.get("/api/test/assignments", controller.allAccess);
+    app.get("/assignments", async (req,res) => {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets",
+        });
+        
+        //Create client instance for Auth
+        const client = await auth.getClient();
+
+        //Instance of Google Sheets API
+        const googleSheets = google.sheets({ version: "v4", auth: client });
+
+        const spreadsheetId = "1c2JDLDsqw27Bwc754dqT3DHQtmM7jgbDM1Wl67LKSRg";
+        //Get Metadata about spreadsheets
+        // const metaData = await googleSheets.spreadsheets.get({
+        //     auth,
+        //     spreadsheetId,
+        // });
+
+        //Get rows about spreadsheets
+        const getRows = await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId,
+            range: "Assignments!A:E",
+        });
+        
+        res.send(getRows.data);
+    });
+
+    app.get("/api/getassignments", async(req, res) => {
+        try {
+        
+            const response = await axios.get(`http://localhost:8080/assignments`);
+        
+            // Mengirim data dari Google Sheets sebagai respons
+            res.json(response.data);
+            } catch (error) {
+            console.error('Error fetching data from Google Sheets:', error);
+            res.status(500).json({ message: 'Internal server error' });
+            }
+    });
 };
